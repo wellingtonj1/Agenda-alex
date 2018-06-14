b67300c839fabc4ce2e83185ea1393d8891d6436
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-06-14 10:12:40 -0300

    Create agenda.h

---
+++ agenda.h
@@ -0,0 +1,22 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+#include <iostream>
+
+using namespace std;
+
+//tera nome endereço telefone email idade bool pr
+
+class agenda
+{
+	private:
+	
+		string nome,endere,email;
+		int idade,telefone;
+		
+	public:
+		
+
+
+};
+#endif
