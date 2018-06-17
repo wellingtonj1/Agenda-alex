a233ef062cbc73412f664dd432cb6cd68849558d
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-06-16 23:37:28 -0300

    Create agenda.cpp

---
+++ agenda.cpp
@@ -0,0 +1,81 @@
+#include "agenda.h"
+#include <iostream>
+
+using namespace std;
+
+void agenda::getagenda()
+{
+    cout<<"\nNome = " << nome
+        << "\n Endereço = "
+        << endere
+        << "\n Email = "
+        << email
+        << "\n Idade = "
+        << idade
+        << "\n telefefone = "
+        << telefone<<endl;
+}
+void agenda::setagenda()
+{
+    setnome();
+    setemail();
+    setende();
+    setidad();
+    setnume();
+
+}
+void agenda::setnome()
+{
+    cout<<"\n\t Digite o nome : ";
+    cin.getline(nome,40);
+}
+void agenda::setende()
+{
+    cout<<"\n\t Digite o endereço : ";
+    cin.getline(endere,80);
+}
+void agenda::setemail()
+{
+    cout<<"\n\t Digite o email : ";
+    cin.getline(email,40);
+}
+void agenda::setidad()
+{
+    int x;
+    cout<<"\n\t Digite a idade : ";
+
+    do
+    {
+        cin>>x;
+        if(x<=0)
+        {
+        cout<<"\nInsira um numero valido";
+        }
+        else
+        {
+            idade=x;
+        }
+    }
+    while(x<=0);
+}
+void agenda::setnume()
+{
+    int num;
+    cout<<"\n\t Digite o telefone com o dd com o numero 9 obrigatorio\n";
+
+    do
+    {
+        cin>>num;
+        if(num<=0||num>99999999999)
+        {
+            cout<<"\nInsira um numero valido";
+        }
+        else
+        {
+            telefone=num;
+        }
+    }
+    while(num<=0||num>99999999999);
+}
+
+
