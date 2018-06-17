b9416ee8b2087ea6174d37970915c52527fbbb0c
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-06-16 23:36:51 -0300

    Update agenda.h

--- agenda.h
+++ agenda.h
@@ -10,50 +10,20 @@ using namespace std;
 class agenda
 {
 	private:
-	
-		string nome,endere,email;
+
+		char nome[40];
+		char endere[80];
+		char email[40];
 		int idade,telefone;
-		
+
 	public:
-		
-		void setagenda()
-		{
-			int x;
-			long long num;
-			cout<<"\n\t Digite o nome\n";
-			getline(cin,nome);
-			cout<<"\n\t Digite o endereço\n";
-			getline(cin,endere);	
-			cout<<"\n\t Digite o email\n";
-			getline(cin,email);	
-			cout<<"\n\t Digite a idade\n";
-			
-			repete:
-			cin>>x;
-			if(x<=0)
-			{
-			cout<<"\nInsira um numero valido";
-			goto repete;
-			}
-			else
-			{
-				idade=x;
-			}
-			cout<<"\n\t Digite o telefone com o dd com o numero 9 obrigatorio\n";
-			
-			repetenum:
-			cin>>num;
-			if(num<=0||num>99999999999)
-			{
-				cout<<"\nInsira um numero valido";
-				goto repetenum;
-			}
-			else
-			{
-				telefone=num;
-			}
-		}
-		
-		
+        void setagenda();
+        void getagenda();
+        void setnome();
+        void setende();
+        void setemail();
+        void setidad();
+        void setnume();
+
 };
 #endif
