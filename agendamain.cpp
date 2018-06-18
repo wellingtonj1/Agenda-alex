8109352d0592b9496a243eae0515bba753a259c8
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-06-18 09:17:40 -0300

    Update agendamain.cpp

--- agendamain.cpp
+++ agendamain.cpp
@@ -9,54 +9,74 @@ using namespace std;
 
 int main()
 {
-    ofstream arq("pessoas.txt",ios::out);
-    if(arq.is_open())
-    {
-    int x,n,lidos;
-    string escolha;
-    agenda* p1;
-    lidos=1;
-    n=0;
-    p1=new agenda[n];
+    fstream arq("Pessoas.nada",ios::out);   
+	if(arq.is_open())
+	{
+	
+		string escolha2;
+		int x,n,lidos;
+		string escolha;
+		agenda* p1;
+		lidos=0;
+		n=0;
+		p1=new agenda[n];
+		
+		
+		do
+		{
+			p1[n].apresenta();
+			cin>>x;
+			
+			switch(x)
+			{
+				case 1:
+				{	
+					
+						do
+						{
+							
+							p1[n].changewind();
+							p1[n].getagenda();
+							arq.write(reinterpret_cast <char*>(&p1[n]),sizeof(agenda));
+							cout<<"\nDeseja Adicionar mais alguma pessoa nesta agenda ? Sim ou NÃ£o : ";
+							cin>>escolha2;
+							n++;
+							lidos++;
+							
+						}
+						while(escolha=="S"||escolha=="s");
+						
+						
+						break;
+				}
+				
+				case 2:
+				{    
+					ifstream arq("Pessoas.nada");
+					for(int i=1;i<lidos;i++)
+					{
+						arq.read(reinterpret_cast <char*>(&p1[i]),sizeof(agenda));
+						
+					}
+					
+				}
+			}
+			cout<<"Deseja realizar outra operaÃ§Ã£o ? S ou N : " ;
+			cin>>escolha;
+			
+		
+		}
+		while(escolha=="s"||escolha=="S");
 
-	do
-    {
-        p1[n].apresenta();
-        cin>>x;
+		delete[]p1;
+		
+		arq.close();
+	}
+	
+	else
+	{
+			cout<<"\n\nO arquivo nÃ£o estÃ¡ aberto!!!\n";
+	}
 
-        switch(x)
-        {
-            case 1:
-            {
-                do
-                {
-                    n++;
-                    lidos++;
-                }
-                while(!p1[n].changewind());
-                break;
-            }
-
-            case 2:
-            {
-                for(int i=1;i<lidos;i++)
-                {
-
-                    arq.write(char*(&p1[i].gettudo()),agenda);
-                }
-            }
-        }
-        cout<<"Deseja realizar outra operação ? S ou N : " ;
-        cin>>escolha;
-        arq.close();
-    }
-    while(escolha=="s"||escolha=="S");
-
-    delete[]p1;
-
-    }
-    else
-    {
-        cout<<"\n\nO arquivo não está aberto!!!\n";
-    }
+    
 }
